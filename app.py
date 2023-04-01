0cab23101d2e7abaf61e638c65d347f464301678
Azuolas3 <ivoskailt@gmail.com>
2023-04-02 01:10:53 +0300

    move search initiation endpoint

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 from flask import Flask, render_template
 import chess
 import chess.engine
+import urllib.parse
 
 app = Flask(__name__)
-#engine = chess.engine.SimpleEngine.popen_uci("./engine/stockfish-windows-2022-x86-64-avx2.exe")
-engine = chess.engine.SimpleEngine.popen_uci("./engine/OutOfStockFish.exe")
+
+
+# engine = chess.engine.SimpleEngine.popen_uci("./engine/stockfish-windows-2022-x86-64-avx2.exe")
+# engine = chess.engine.SimpleEngine.popen_uci("./engine/OutOfStockFish.exe")
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('home.html')
 
-@app.route('/testengine')
-def test_engine():
-    board = chess.Board()
-    result = engine.play(board, chess.engine.Limit(time=1))
+
+@app.route('/engine/<path:fen>')
+def test_engine(fen):
+    engine = chess.engine.SimpleEngine.popen_uci("./engine/OutOfStockFish.exe")
+    decoded_fen = urllib.parse.unquote(fen)
+    board = chess.Board(decoded_fen)
+    result = engine.play(board, chess.engine.Limit(time=3))
+    engine.close()
     return result.move.uci()
 
 
