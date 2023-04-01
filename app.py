d2772872fa1755ac3b01478b05b7a3166d20e639
Azuolas3 <ivoskailt@gmail.com>
2023-04-01 20:56:17 +0300

    OutOfStockFish chess engine integration, basic test

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+import chess
+import chess.engine
 
 app = Flask(__name__)
+#engine = chess.engine.SimpleEngine.popen_uci("./engine/stockfish-windows-2022-x86-64-avx2.exe")
+engine = chess.engine.SimpleEngine.popen_uci("./engine/OutOfStockFish.exe")
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('home.html')
+
+@app.route('/testengine')
+def test_engine():
+    board = chess.Board()
+    result = engine.play(board, chess.engine.Limit(time=1))
+    return result.move.uci()
 
 
 if __name__ == '__main__':
